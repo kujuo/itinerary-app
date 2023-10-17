e539ea0197004b4223aa1a5d4a157e2acb46b2e8
kujuo <61703000+kujuo@users.noreply.github.com>
2023-10-16 22:19:33 -0400

    Initial Commit

---
+++ itinerary-app/itinerary_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  itinerary_appApp.swift
+//  itinerary-app
+//
+//  Created by jhou on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct itinerary_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
